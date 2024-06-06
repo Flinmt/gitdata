5603d417daf975a5b09f34c7896f36b95f9546fa
Vitor José <vitorlimagerr.profissional@gmail.com>
2024-06-06 17:51:46 -0300

    feat: redesing profile card

--- assets/scripts/gitApi.js
+++ assets/scripts/gitApi.js
@@ -2,44 +2,47 @@ const user = document.getElementById('gitUser')
 const main = document.getElementById('card')
 const url = "https://api.github.com/users/"
 
-
 async function getUser() {
     try {
         const response = await fetch(url + user.value);
         const data = await response.json();
+        console.log(data);
 
+        main.style.display = "flex"; 
         if (response.status === 200) {
             const cardHtml = `
-            <a href="${data.html_url}" target="_blank" class="card" id="card">
-                <section class="card-top">
-                    <img class="profile-photo" src="${data.avatar_url}" alt="profile-photo">
-                    <div class="top-info">
-                        <h4>${data.name}</h4>
-                        <div class="info local">
-                            <img src="./assets/imgs/map-pin.svg" alt="map-pin">
-                            <span>${data.location}</span>
-                        </div>
-                        <div class="info">
-                            <img src="./assets/imgs/users.svg" alt="users">
-                            <span>${data.followers}</span>
-                        </div>
-                    </div>
-                </section>
-            </a>
+            <div class="profile-img">
+                <img src="${data.avatar_url}" alt="profile-photo">
+            </div>
+            <div class="name">
+                <h3>${data.name}</h3>
+                <span>${data.login}</span>
+            </div>
+            <ul class="info">
+                <li>
+                    <label>Repositories</label>
+                    <span>${data.public_repos}</span>
+                </li>
+                <li style="margin-left: -12px;">
+                    <label>Followers</label>
+                    <span>${data.followers}</span>
+                </li>
+                <li>
+                    <label>Following</label>
+                    <span>${data.following}</span>
+                </li>
+            </ul>
+            <textarea class="bio" rows="3" cols="50" readonly>${data.bio}</textarea>
+            <a href="${data.html_url}" target="_blank" class="link-profile">View Profile</a>
         `
 
             main.innerHTML = cardHtml;
             return
-        }
-
-        const cardHtml = `
-            <a href="./index.html" class="card" id="card">
-                ${data.message}
-            </a>
-        `
+        } else {
+            const cardHtml = `${data.message}, Sorry &#128557`
 
         main.innerHTML = cardHtml;
-
+        }
     } catch (e) {
         console.log(e);
 
